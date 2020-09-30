8c90e0c78746661fbc9a984e23537c4209fd279b
NicolasCastroGarcia <nicolascastrogarcia@gmail.com>
2020-09-30 11:18:39 -0300

    implementing mercado pago subscriptions

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+
+const app = express();
+
+const SubscriptionController = require("./controllers/SubscriptionController");
+//importamos el controller
+
+const SubscriptionService = require("./services/SubscriptionService");
+
+const SubscriptionInstance = new SubscriptionController(
+  new SubscriptionService()
+);
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/subscription/new", (req, res) =>
+  SubscriptionInstance.getMercadoPagoSubscriptionLink(req, res)
+);
+
+module.exports = app;
---
+++ services/SubscriptionService.js
@@ -0,0 +1,49 @@
+const MercadoPago = require("mercadopago");
+
+class SubscriptionService {
+  constructor() {
+    MercadoPago.configure({
+      client_id: "client_id",
+      client_secret: "client_secret"
+    });
+  }
+
+  async getSubscriptionLink() {
+    const preference = {
+      payer_email: "test@gmail.com",
+      //email del usuario comprador
+      reason: "prueba de subscripción",
+      external_reference: "",
+      back_url: "https://www.mipaginaweb.com/gracias",
+      //si se completa el pago
+      auto_recurring: {
+        //objeto para crear la subscripción
+        frequency: 1,
+        // frecuencia de cobro
+        frequency_type: "months",
+        //tipo de frecuencia
+        //en este ejemplo es 1 vez al mes
+        transaction_amount: 100,
+        //precio de la suscripción
+        currency_id: "ARS"
+        //moneda a cobrar
+      }
+    };
+
+    try {
+      const mp = await MercadoPago.preapproval.create(preference);
+      //usamos el sdk de mercado pago
+
+      const linkCheckout = mp && mp.response && mp.response.init_point;
+      //obtenemos el link
+
+      return linkCheckout;
+      //le devolvemos el link al controller
+    } catch (err) {
+      console.log(err);
+      return false;
+    }
+  }
+}
+
+module.exports = SubscriptionService;
